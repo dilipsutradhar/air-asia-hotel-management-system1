51069631b58cef1d7ce0f7d9488c974e5831233f
dilip <dilip@DILIP-PC>
2020-08-23 21:33:02 +0530

    checkin another

--- air-asia-hotel-management-system1/src/main/java/com/air/org/asia/AirAsiaHMSApplication.java
+++ air-asia-hotel-management-system1/src/main/java/com/air/org/asia/AirAsiaHMSApplication.java
@@ -1,7 +1,6 @@
 package com.air.org.asia;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
